77c4067dba9421614a8deb5524b02721a391e8de
mapc <mapc@getui.com>
2016-06-21 15:55:51 +0800

    ORM mapper dao model add

---
+++ src/main/java/com/songtaste/entity/MusicTypeRelation.java
@@ -0,0 +1,43 @@
+package com.songtaste.entity;
+
+public class MusicTypeRelation {
+    private Long id;
+
+    private Long musicId;
+
+    private Long typeId;
+
+    private Integer isDeleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMusicId() {
+        return musicId;
+    }
+
+    public void setMusicId(Long musicId) {
+        this.musicId = musicId;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
---
+++ src/main/java/com/songtaste/entity/UserGroup.java
@@ -0,0 +1,55 @@
+package com.songtaste.entity;
+
+import java.util.Date;
+
+public class UserGroup {
+    private Long id;
+
+    private String groupName;
+
+    private String creator;
+
+    private Date createTime;
+
+    private Integer isDeleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName == null ? null : groupName.trim();
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
