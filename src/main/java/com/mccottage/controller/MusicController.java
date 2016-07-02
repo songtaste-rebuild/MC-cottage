ed0c460834fc0e00837f5a4857d06fa061b1efbc
micro <pengmi1@126.com>
2016-07-02 14:11:18 +0800

    fix

--- src/main/java/com/mccottage/controller/MusicController.java
+++ src/main/java/com/mccottage/controller/MusicController.java
@@ -1,17 +1,12 @@
 package com.mccottage.controller;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
