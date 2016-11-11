02e377fbda7275e9d866b5583bb324f500360c9e
micro <mapc@getui.com>
2016-11-11 19:37:56 +0800

    task job

---
+++ src/main/java/com/mccottage/job/TestJob.java
@@ -0,0 +1,28 @@
+package com.mccottage.job;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TestJob {
+	@Scheduled(cron = "*/10 * * * * ?")
+	public void execute() {
+		System.out.println("start job...");
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 100, TimeUnit.MINUTES, new LinkedBlockingDeque());
+		executor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				for (int i = 0;i < 100;i ++) {
+					System.out.println("star to this thread : " + Thread.currentThread().getName() + " i = " + i);
+				}
+			}
+		});
+		System.out.println("main end job");
+	}
+}
