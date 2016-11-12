bf3e1726460f204dd48ca4e0fec38750c08af989
马培川 <pengmi1@126.com>
2016-11-12 21:01:54 +0800

    add aop jar

--- src/main/java/com/mccottage/aop/MusicAdvisor.java
+++ src/main/java/com/mccottage/aop/MusicAdvisor.java
@@ -9,17 +9,15 @@ import org.springframework.aop.MethodBeforeAdvice;
  * this programe writted by micro
  */
 
-public class MusicAdvisor implements MethodBeforeAdvice, AfterReturningAdvice{
+public class MusicAdvisor{
 
 	// 对应函数执行之后的返回
-	@Override
 	public void afterReturning(Object returnValue, Method method,
 			Object[] args, Object target) throws Throwable {
 		System.out.println("music play over...");
 	}
 
 	// 对应函数执行之前
-	@Override
 	public void before(Method method, Object[] args, Object target)
 			throws Throwable {
 		System.out.println("music play start...");
